9c1c95a9ad58b38f83aee01ef8768254795caeac
Сергей <gan7er@yandex.ru>
2021-04-28 20:36:33 +0300

    Массив

--- Task-4-1/Task-4-1/Program.cs
+++ Task-4-1/Task-4-1/Program.cs
@@ -4,9 +4,17 @@ namespace Task_4_1
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine();
+            string[] a=new string[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                a[i] = "stroka";
+            }
+            a[6] = "gb";
+            Console.WriteLine($"{a[5]},{a[9999]},{a[6]}");
         }
     }
 }
