3c407aa0c20b046a43ef0901ca0bd19002ed94cc
Сергей <gan7er@yandex.ru>
2021-05-01 12:18:31 +0300

    Тест бенч не получается

--- Task-4-1/Task-4-1/Program.cs
+++ Task-4-1/Task-4-1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
 
 
 namespace Task_4_1
@@ -7,15 +10,18 @@ namespace Task_4_1
    class TestCase
     {
        public int Count;
+       //public string[] arr;
+       //public HashSet<string> hash;
+       //public  string str = "stroka5999";
     }
-
-
-  
-
-    class Program
+    public  class ArrHash
     {
-      
-        public static string[] FillArray()
+        public  string[] arr { get; set; }
+        public  HashSet<string> hash { get; set; }
+        public  string str = "stroka5999";
+
+        
+        public  string[] FillArray()
         {
             string str = "stroka";
             string[] a = new string[10000];
@@ -25,57 +31,87 @@ namespace Task_4_1
             }
             return a;
         }
-        public static HashSet<string> FillHash()
+        public  HashSet<string> FillHash()
         {
             string str = "stroka";
-           var hashSet = new HashSet<string>();
+            var hashSet = new HashSet<string>();
             for (int i = 0; i < 10000; i++)
             {
                 hashSet.Add(str + i);
             }
             return hashSet;
         }
-        public static void SearchArray(string[]a,string search)
+        public  bool SearchArray(string[] a, string search)
         {
             for (int i = 0; i < a.Length; i++)
             {
                 if (a[i] == search)
                 {
-                    Console.WriteLine($"Найдена строка {search}");
+                    return true;
                 }
 
-            }
+            }return false;
 
         }
-        public static void SearchHash(HashSet<string> hashSet,string search)
+        public  bool SearchHash(HashSet<string> hashSet, string search)
         {
             if (hashSet.Contains(search))
             {
-                Console.WriteLine($"Найдена строка {search}");
+                return true;
             }
-            
+            return false;
+
 
         }
-        public static void TestFill(TestCase testCase)
+        [Benchmark]
+        public  void TestArBench()
         {
-            if (testCase.Count == FillArray().Length&&testCase.Count==FillHash().Count)
-            {
-                Console.WriteLine("VALID TEST");
-            }
-            else
-            {
-                Console.WriteLine("INVALID TEST");
-            }
+            arr = FillArray();
+           
+            SearchArray(arr,str);
 
         }
-        
-       
+        [Benchmark]
+        public  void TestHashBench()
+        {
+            hash = FillHash();
+            
+            SearchHash(hash,str);
 
+        }
+
+
+
+
+    }
+
+    class Program
+    {
+      
+        //public void TestFill(TestCase testCase)
+        //{
+        //    if (testCase.Count == FillArray().Length&&testCase.Count==FillHash().Count)
+        //    {
+        //        Console.WriteLine("VALID TEST");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("INVALID TEST");
+        //    }
+
+        //}
+        
         static void Main(string[] args)
         {
-            TestCase testCase = new TestCase { Count = 10000 };
-            TestFill(testCase);
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+           
 
+            //arrHash.arr = arrHash.FillArray();
+            //arrHash.hash = arrHash.FillHash();
+
+           // Console.ReadKey();
+            
+            
 
         }
     }
