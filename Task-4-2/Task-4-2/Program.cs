691f5704c7725da4cde2ddd4043288d497cd723b
Сергей <gan7er@yandex.ru>
2021-05-17 16:43:49 +0300

    Осталось Print,Test

--- Task-4-2/Task-4-2/Program.cs
+++ Task-4-2/Task-4-2/Program.cs
@@ -2,22 +2,32 @@
 
 namespace Task_4_2
 {
-    public class TreeNode
+
+    public interface ITree
     {
-        public int Value { get; set; }
-        public TreeNode LeftChild { get; set; }
-        public TreeNode RightChild { get; set; }
-        public override bool Equals(object obj)
-        {
-            var node = obj as TreeNode;
-
-            if (node == null)
-                return false;
-
-            return node.Value == Value;
-
-        }
+        TreeNode GetRoot();
+        void AddItem(int value); // добавить узел
+        void RemoveItem(int value); // удалить узел по значению
+        TreeNode GetNodeByValue(int value); //получить узел дерева по значению
+        void PrintTree(); //вывести дерево в консоль
     }
+
+    //public class TreeNode
+    //{
+    //    public int Value { get; set; }
+    //    public TreeNode LeftChild { get; set; }
+    //    public TreeNode RightChild { get; set; }
+    //    public override bool Equals(object obj)
+    //    {
+    //        var node = obj as TreeNode;
+
+    //        if (node == null)
+    //            return false;
+
+    //        return node.Value == Value;
+
+    //    }
+    //}
 
     public class NodeInfo
     {
